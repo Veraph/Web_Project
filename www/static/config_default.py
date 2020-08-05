2a7ddf4c0f2f29745667059bed7b017c3857ee0c
Veraph <jmw3531@live.com>
2020-08-05 22:08:47 +0100

    add configurations.

---
+++ www/static/config_default.py
@@ -0,0 +1,20 @@
+#!usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''Default configurations.'''
+
+__author__ = 'Veraph'
+
+configs = {
+    'debug': True,
+    'db': {
+        'host': '127.0.0.1',
+        'port': 3333,
+        'user': 'www',
+        'password': 'www',
+        'db': 'test1'
+    },
+    'session': {
+        'secret': 'What?'
+    }
+}
